94db8586ef27d95871fe17d6ee23eac46c72ec6b
scollet <origamillamaa@gmail.com>
2017-10-07 13:59:51 -0700

    ok

---
+++ main.js
@@ -0,0 +1,22 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   main.js                                            :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: scollet <marvin@42.fr>                     +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2017/10/07 13:10:04 by scollet           #+#    #+#             //
+//   Updated: 2017/10/07 13:59:09 by scollet          ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+var record = require('node-record-lpcm16')
+var fs = require('fs')
+ 
+var file = fs.createWriteStream('test.wav', { encoding: 'binary' })
+ 
+record.start({
+	  sampleRate : 44100,
+  verbose : true
+})
+.pipe(file)
