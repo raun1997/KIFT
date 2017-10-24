53f9cf9a0ddeb9f74fd9ab408d0870370818fecd
Elliot Tregoning <etregoni@e1z3r2p6.42.us.org>
2017-10-24 00:12:48 -0700

    added coountdown timer

---
+++ src/server/cmds/nukes.sh
@@ -0,0 +1,37 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    nukes.sh                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: etregoni <etregoni@student.42.us.org>      +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2017/10/23 21:35:44 by etregoni          #+#    #+#              #
+#    Updated: 2017/10/23 21:35:48 by etregoni         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+while true ; do
+
+    say -v "Trinoids" 10
+	sleep 1
+	say -v "Trinoids" 9
+	sleep 1
+	say -v "Trinoids" 8
+	sleep 1
+	say -v "Trinoids" 7
+	sleep 1
+	say -v "Trinoids" 6
+	sleep 1
+	say -v "Trinoids" 5
+	sleep 1
+	say -v "Trinoids" 4
+	sleep 1
+	say -v "Trinoids" 3
+	sleep 1
+	say -v "Trinoids" 2
+	sleep 1
+	say -v "Trinoids" 1
+	sleep 1
+	say -v "Trinoids" Nukes launched
+	break
+done
